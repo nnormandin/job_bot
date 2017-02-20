e344017d36bdb0a9b9003f0744fb58935537823a
nnormandin <nicholas.e.normandin@gmail.com>
2017-02-20 12:48:50 -0500

    TODOs in Search class

--- job_bot/search.py
+++ job_bot/search.py
@@ -8,6 +8,17 @@ class Search(object):
 	def result(self):
 		path = "//div[@class='search-result__wrapper']"
 		self.result = self._browser.find_elements_by_xpath(str(path))
+
+		#todo:
+			# move result function to init
+			# Search.results object is list of results
+			# Result class provides necessary functions / attributes
+				# Result.name
+				# Result.company
+				# Result.link
+				# open in new tab
+				# scroll on page
+				# record name and occupation to log file
+
 		return(self.result)
 
-	
