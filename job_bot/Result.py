294698723bca4ba7f434e5543963fa9e5763a652
nnormandin <nicholas.e.normandin@gmail.com>
2017-02-21 16:55:24 -0500

    add visit function

--- job_bot/Result.py
+++ job_bot/Result.py
@@ -1,12 +1,23 @@
 # Result class
+import time
+from job_bot.helpers import *
 
 class Result(object):
 
-	def __init__(self, element):
-		name_element = element.find_element_by_class_name("name")
-		self.name = name_element.text
-		selector = "p[class*='subline-level-1']"
-		self.company = element.find_element_by_css_selector(str(selector)).text
-		
+	def __init__(self, element, browser):
 
-		#self.link = 
+		## TODO: make this work for jobs w/ conditional
+		self._browser = browser
+
+		# select name element and name element text
+		self.name_element = element.find_element_by_class_name("name")
+		self.name = self.name_element.text
+		
+		# select company name / job title
+		co_selector = "p[class*='subline-level-1']"
+		self.company = element.find_element_by_css_selector(str(co_selector)).text
+
+	def visit(self, delay = 5):
+		self.name_element.click()
+		wait_load()
+		time.sleep(delay)
