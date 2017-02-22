e393539b02b421b2072e6c26864f6d8f772e1479
nnormandin <nicholas.e.normandin@gmail.com>
2017-02-21 21:53:28 -0500

    new helper script for search object

---
+++ job_bot/search_helpers.py
@@ -0,0 +1,17 @@
+from job_bot.Result import Result
+from job_bot.helpers import *
+
+
+def recalculate_results(browser):
+
+	out = []
+	elements = search_results(browser)
+
+	if elements:
+		for i in elements:
+			try:
+				out.append(Result(i, browser))
+			except:
+				print("-- error occurred")
+
+	return(out)
